8c3ac4f6fc6d4ac6c9af3c68db9bdfd06451dbbe
Yogesh bhatt <bhattyogesh700@gmail.com>
2023-03-16 19:20:23 +0200

    site is now responsive

--- static/js/script.js
+++ static/js/script.js
@@ -33,21 +33,6 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Coming Soon"," Under Development","Under Construction"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["Student", "Developer", "Blogger"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
