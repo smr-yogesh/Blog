2bccf3030fb8e0cdcd7004bb196c82146de9f2f1
Yogesh bhatt <bhattyogesh700@gmail.com>
2023-05-22 23:09:08 +0300

    now able to make searches

--- routes/posts.py
+++ routes/posts.py
@@ -5,6 +5,14 @@ from model.post import blogpost
 
 posts_B = Blueprint('posts_B', __name__)
 
+#This returns posts after being selected on index or from admin page.
+@posts_B.route('/post/<int:post_id>')
+def post(post_id):
+    post = blogpost.query.filter_by(id=post_id).one()
+
+    return render_template('post.html', post=post)
+
+#post adding route
 @posts_B.route('/addpost')
 def a_p():
     if "user" in session:
@@ -13,6 +21,7 @@ def a_p():
     flash("! Please login first !")
     return redirect(url_for('B_user.register', mode='login'))
 
+#sends post to database.
 @posts_B.route('/ap', methods=['POST'])
 def ap():
     title = request.form['title']
@@ -27,12 +36,14 @@ def ap():
 
     return redirect(url_for('admin_B.admin'))
 
+#Post editing route.
 @posts_B.route('/edit/', methods=['POST','GET'])
 def edit():
     post_id = request.form['edit_id']
     post_to_edit = blogpost.query.filter_by(id=post_id).one()
     return render_template ('updatepost.html', post=post_to_edit)
 
+#Updates/edits post
 @posts_B.route('/update', methods=['POST'])
 def update():
     post_id = request.form['edit_id']
@@ -40,10 +51,6 @@ def update():
     author = request.form['author']
     content = request.form['content']
     word = content.split()
-  #  if word[0] == "[Edited]":
-  #      pass
-  #  else:
-  #      content = "[Edited]&emsp;" + content
 
     post = blogpost.query.filter_by(id=post_id).one()
     post.title = title
@@ -54,6 +61,7 @@ def update():
     flash("Edited successfully!!","message-success")
     return redirect(url_for('admin_B.admin'))
 
+#Delete post
 @posts_B.route('/delete', methods=['POST'])
 def delete():
     id  = request.form['del_id']
@@ -65,3 +73,25 @@ def delete():
     except:
         flash("something went wrong!!")
     return redirect(url_for('admin_B.admin'))
+
+#Post Search
+@posts_B.route('/search', methods=['POST','GET'])
+def search():
+    search = request.form['search']
+    post_ids = []
+
+    if "user" in session:
+        user_i = session["user_id"]
+        posts = blogpost.query.filter_by(user_id=user_i).all()
+        for post in posts:
+            if search.lower() in post.title.lower():
+                post_ids.append(post.id)
+        result = blogpost.query.filter(blogpost.id.in_(post_ids)).all()
+        return render_template('index.html', posts=result)
+
+    posts = blogpost.query.all()
+    for post in posts:
+        if search.lower() in post.title.lower():
+            post_ids.append(post.id)
+    result = blogpost.query.filter(blogpost.id.in_(post_ids)).all()
+    return render_template('index.html', posts=result)
